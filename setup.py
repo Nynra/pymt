b5b6ba84bdac79f051312bbabdb94ead51143d32
nynra <nynradev@pm.me>
2024-02-20 13:10:36 +0100

    replaced the toml with a setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="pymt",
+    version="0.0.6",
+    author="Nynra",
+    description="A module for Merkle Tries and proofs.",
+    py_modules=["pymt"],
+    package_dir={"": "src"},
+    install_requires=["cytoolz", "eth-hash", "eth-typing", 
+                      "eth-utils", "pycryptodome", "rlp", "toolz"],
+)
